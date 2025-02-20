9cb8fe5c0902ab657a9c03db41c90f7ac14eae59
Saurabh Bhatnagar <saurabh.bhatnagar01@sap.com>
2025-02-20 23:22:24 +0530

    init

--- app.js
+++ app.js
@@ -1,257 +1,77 @@
-var express = require('express');
-var bodyParser = require('body-parser');
-var mongoose = require('mongoose');
-var morgan = require('morgan');
-var methodOverride = require('method-override');
+const express = require("express");
+const path = require("path");
+const app = express();
+const port = 3000;
 
-var app = express();
-
-//Constants for Waiting list Threshold.
-const waitingListMin = 80;
-const waitingListMax = 170;
-
-// REPLACE THE LINE BELOW TO ADD MONGO DB CONNECTION STRING
-mongoose.connect('mongodb://r3nwsa9dhoq7hb40:ratm745ilgtrok3b@10.11.241.3:44923/ew0nt2x4gifseont'); //Mongo DB instance
-
-app.use(express.static(__dirname + '/public'));
-app.use(morgan('dev'));
-app.use(bodyParser.urlencoded({'extended':'true'}));
-app.use(bodyParser.json());
-app.use(bodyParser.json({ type: 'application/vnd.api.json' }));
-app.use(methodOverride());
-
-var RegisterSchema = require('./models/Register.js').RegisterSchema;
-var Registrations = mongoose.model('Registration', RegisterSchema);
-var crosswordClues = require('./models/CrosswordClues.js').Clues;
-
-//get all registrations
-app.get('/api/registrations', function(req, res) {
-	// use mongoose to get all todos in the database
-	Registrations.find(function(err, registrations) {
-		// if there is an error retrieving, send the error.
-		// nothing after res.send(err) will execute
-		if(err)
-			res.send(err);
-		
-		res.json(registrations); //return all registrations in JSON format
-	});
+app.get("/", (req, res) => {
+  res.send(`
+        <!DOCTYPE html>
+        <html lang="en">
+        <head>
+            <meta charset="UTF-8">
+            <meta name="viewport" content="width=device-width, initial-scale=1.0">
+            <title>Crossword Embed</title>
+            <script src="https://unpkg.com/@ui5/webcomponents/dist/Button.js"></script>
+            <script src="https://unpkg.com/@ui5/webcomponents/dist/Title.js"></script>
+            <style>
+                body { 
+                    display: flex; 
+                    flex-direction: column;
+                    justify-content: center; 
+                    align-items: center; 
+                    height: 100vh; 
+                    background-color: #f4f4f4; 
+                    margin: 0;
+                    font-family: "72", "72full", Arial, Helvetica, sans-serif;
+                }
+                iframe {
+                    border: 3px solid black; 
+                    display: block;
+                    width: 800px;
+                    height: 800px;
+                }
+                .footer {
+                    margin-top: 20px;
+                    font-size: 16px;
+                    color: #fff;
+                    background-color: #354a5f;
+                    padding: 10px;
+                    text-align: center;
+                    width: 100%;
+                    font-weight: bold;
+                    border-top: 3px solid #fff;
+                }
+                .sap-banner {
+                    width: 100%;
+                    background-color: #354a5f;
+                    color: white;
+                    padding: 10px;
+                    display: flex;
+                    align-items: center;
+                    justify-content: center;
+                    font-size: 20px;
+                    font-weight: bold;
+                }
+                .sap-banner img {
+                    height: 30px;
+                    margin-right: 10px;
+                }
+            </style>
+        </head>
+        <body>
+            <div class="sap-banner">
+                <img src="https://upload.wikimedia.org/wikipedia/commons/5/59/SAP_2011_logo.svg" alt="SAP Logo">
+                Week 1 Crossword Challenge
+            </div>
+            <iframe frameborder="0" src="https://crosswordlabs.com/embed/cis-knowledge-hub-crossword-week-1"></iframe>
+            <div class="footer">Made with ❤️ at CIS Knowledge Café☕ by Saurabh</div>
+        </body>
+        </html>
+    `);
 });
 
-//create registration and send back all registrations after creation
-app.post('/api/registrations', function(req, res) {
-	//Validate the incoming request before making the registration.
-	var validationObj = req.body;
-	console.log(req.body);
-	if(req.body.cw0 == null || typeof(req.body.cw0) == 'undefined' || req.body.cw20 == null || typeof(req.body.cw0) == 'undefined'){
-		console.log("Cross Word not solved! Sending Error");
-		return res.status(400).json({"code":"400", "message":"Please solve the crossword & get atleast 5 right for successful registration."});
-	} else {
-		req.url = '/api/validate';
-		req.body = validationObj;
-		validationResponse = validateCrossWord(req);
+app.use(express.static(path.join(__dirname, "public")));
 
-		//Check the Validation Response and register only if there are no validation errors.
-		switch(validationResponse.code){
-			case "200":  //Continue the DB update.
-				break;
-			case "400":
-				return res.status(400).json(validationResponse);
-		}
-	}
-
-	// create a registration, information comes from AJAX request from Angular
-	Registrations.create({
-		name : req.body.name.toUpperCase(),
-		inumber : req.body.inumber,
-		mailid : req.body.mailid,
-		message : validationResponse.validationmessage,
-		status : "Success",
-		regtime : (new Date()).getTime()
-	}, function(err, registration) {
-		if (err) {
-			console.log(err);
-			res.status(400).json({"code" : "400","message" : err});
-			//res.send(err);			
-		} else {
-			//get and return all the registrations after you create another
-			Registrations.find(function(err, registrations) {
-				if(err) {
-					console.log(err);
-					var error = {
-						"code" : "1002",
-						"message" : "Something went wrong!"
-					};
-					res.status(400).json({"code" : "400", "message" : error});
-				}
-				console.log("Number of registrations::::: " + registrations.length);
-				if(registrations.length > waitingListMin && registrations.length <= waitingListMax) {
-				    var conditions = { _id: registration._id }
-				      , update = { status: 'waitinglist' }
-				      , options = {};
-				
-					Registrations.update(conditions, update, options, function(err, registration) {
-						if(err) {
-							console.log(err);
-							var error = {
-								"code" : "1002",
-								"message" : "Something went wrong!"
-							};
-							res.status(400).json({"code" : "400", "message" : error});
-						}
-					});
-					var error = {
-						"code" : "1000",
-						"message" : "You are on the waiting list!"
-					};
-					res.status(400).json({"code" : "400", "message" : error});
-				} else if(registrations.length > waitingListMax) {
-				    var conditions = { _id: registration._id }
-				      , update = { status: 'closed' }
-				      , options = { multi: true };
-				
-					Registrations.update(conditions, update, options, function(err, numAffected) {
-						if(err) {
-							console.log("Failed to update row " + registration._id + " ::  " + err);
-							var error = {
-								"code" : "1002",
-								"message" : "Something went wrong!"
-							};
-							res.status(400).json({"code" : "400", "message" : error});
-						} else {
-							console.log("Updation of row " + registration._id + " was successful");
-						}
-					});
-					var error = {
-						"code" : "1001",
-						"message" : "Registrations are closed!"
-					};
-					res.status(400).json({"code" : "400", "message" : error});
-				} else {
-					res.status(200).json({"code":"201", "message":"Successfully registered"});	
-				}
-			});			
-		}		
-	});
+app.listen(port, () => {
+  console.log(`Server is running at http://localhost:${port}`);
 });
-
-app.post('/api/validate', function (req, res) {
-	
-	validateResult = validateCrossWord(req);
-	res.status(validateResult.code).json(validateResult);
-
-});
-
-function validateCrossWord(req){
-
-	validateResult = {};
-
-	var right = 0;
-	var wrong = 0;
-
-	console.log("CROSSWORD LENGTH:::::::: "+crosswordClues.length);
-
-
-
-	var i = 0;
-	for (i; i< crosswordClues.length; i++) {
-		console.log(i+" ::::: "+req.param("cw"+i));
-		if(crosswordClues[i] == req.param("cw"+i)) {
-			right++;
-			console.log("right:::: "+crosswordClues[i]);
-		} else {
-			wrong++;
-			console.log("wrong::::: "+crosswordClues[i]);
-		}
-	}
-	if(right == crosswordClues.length && wrong == 0) {
-		console.log("Sending success");
-		validateResult = {"code":"200", "validationmessage":"Crossword successfully solved"};
-	} else if (right >= 5) {
-		console.log("Completed with few errors");
-		validateResult = {"code":"200", "validationmessage":right+" Correct; "+wrong+" Incorrect"};
-	} else {
-		console.log("More errors, sending error");
-		validateResult = {"code":"400", "validationmessage":right+" correct answers. Get atleast 5 right for successful registration but maximum possible to have a better chance."};
-	}
-
-	return validateResult;
-}
-
-//delete a registration
-app.delete('/api/registrations/:registration_id', function(req, res) {
-	Registrations.remove({
-		_id : req.params.registration_id
-	}, function(err, registration) {
-		if(err)
-			res.send(err);		
-		// get and return all the registration after you delete this
-		Registrations.find(function(err, registrations) {
-			if(err)
-				res.send(err);
-			res.json(registrations);	
-		});
-	});
-});
-
-/* 
- * Exposing this end point publicly is dangerous!
- * Uncomment only when necessary
- */ 
- /*
-app.delete('/api/resetdata', function(req, res) {
-	Registrations.remove({}, function(err) {
-		if(err)
-			res.send(err);
-		// get and return all the registration after you delete this
-		Registrations.find(function(err, registrations) {
-			if(err)
-				res.send(err);
-			res.json(registrations);	
-		});
-	});
-});*/
-
-// application ------------------------
-app.get('/', function(req, res) {
-	Registrations.find(function(err, registrations) {
-		if (err)
-			res.send(err);
-		if(registrations.length < waitingListMax) {
-			res.sendfile('./public/register.html');
-		} else {
-			res.sendfile('./public/registrationsclosed.html');
-		}
-	});
-});
-
-app.get('/registrations', function(req, res) {
-	res.sendfile('./public/registrations.html');
-});
-
-app.get('/register', function(req, res) {
-	Registrations.find(function(err, registrations) {
-		if (err)
-			res.send(err);
-		if(registrations.length < waitingListMax) {
-			res.sendfile('./public/register.html');
-		} else {
-			res.sendfile('./public/registrationsclosed.html');
-		}
-	});
-});
-
-/* 
- * Exposing this end point publicly is dangerous!
- * Uncomment only when necessary
- */ 
- /*
-app.get('/reset', function(req, res) {
-	res.sendfile('./public/resetdata.html');
-});*/
-
-// listen (start app with node app.js) ===================
-var PORT = process.env.PORT || 5050;
-app.listen(PORT);
-console.log("App listening on port " + PORT);
-
-
--- models/CrosswordClues.js
+++
@@ -1,4 +0,0 @@
-exports.Clues = ["SMARTSCAPE", "RUM", "BACKTRACE", 
-                 "HOTSPOTS", "PAASTOKEN", "AI", 
-				 "SERVICEFLOW", "ONEAGENT", "PUREPATH",
-				 "DYNATRACE", "TAGS"]
