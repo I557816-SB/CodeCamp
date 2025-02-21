c29137ca3b4b74d106c7daa548944e2d79abf225
Saurabh Bhatnagar <saurabh.bhatnagar01@sap.com>
2025-02-21 06:59:52 +0530

    two

--- app.js
+++ app.js
@@ -1,7 +1,10 @@
 const express = require("express");
 const path = require("path");
+const cors = require("cors"); // Import the cors package
 const app = express();
 const port = 3000;
+
+app.use(cors()); // Use the cors middleware
 
 app.get("/", (req, res) => {
   res.send(`
